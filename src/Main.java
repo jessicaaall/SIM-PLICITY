52700d4846b8f8a818508fb475950a4dc5ef023c
ZeevHaydar <nzhifhaidar@gmail.com>
2023-04-14 15:57:29 +0700

    Create Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+public class Main {
+    public static void main(String[] args) {
+        GameFrame frame = new GameFrame();
+        frame.setVisible(true);
+    }
+}
+
