06ac88a63c227ed8f6720bf23920aca0bc13375f
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-04-18 11:22:23 +0700

    Buat Sim.java, Inventory.java, dan Waktu.java
    Udah ada atribut SIm.java

---
+++ src/entity/Sim.java
@@ -0,0 +1,27 @@
+package entity;
+
+import java.util.*;
+
+public class Sim {
+    // Deklarasi Variabel
+    private String namaLengkap;
+    private Pekerjaan pekerjaan;
+    private int uang;
+    private Inventory inventory;
+    private int kekenyangan;
+    private int mood;
+    private int kesehatan;
+    private String status;
+    private Ruangan lokasi;
+    private boolean isDuduk;
+    private ArrayList<Rumah> kepemilikanRumah;
+    private int waktuTidur;
+
+    // Konstruktor
+
+    // Method : Getter
+
+    // Method : Setter
+    
+    // Method : Until Sim
+}
