f41c77a39eaf9b8f2beaaf2edaa0460a5d37219a
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-04-18 16:30:11 +0700

    Update Sim.java dan Inventory.java

--- src/entity/Inventory.java
+++ src/entity/Inventory.java
@@ -1,5 +1,12 @@
 package entity;
 
+import java.util.*;
+
 public class Inventory {
-    
+    // Deklarasi Atribut
+    private Map<Objek, String> container;
+
+    // Konstruktor
+
+    // Method
 }
