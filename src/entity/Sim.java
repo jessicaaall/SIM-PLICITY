64b78bedf34ff66288f22d89a4c58f85bf055494
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-04-18 11:58:57 +0700

    Update Sim.java

--- src/entity/Sim.java
+++ src/entity/Sim.java
@@ -17,11 +17,64 @@ public class Sim {
     private ArrayList<Rumah> kepemilikanRumah;
     private int waktuTidur;
 
-    // Konstruktor
+    // Konstruktor (Memakai singleton pattern)
+    private Sim() {} // Buat konstruktor Sim private
+    private static Sim instanceSim = new Sim(); // Buat objek Simnya
+    public static Sim getInstanceSim() { // Buat method buat return Simnya aja
+        return instanceSim;
+    }
 
     // Method : Getter
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+    public Pekerjaan getPekerjaan() {
+        return pekerjaan;
+    }
+    public int getUang() {
+        return uang;
+    }
+    public Inventory getInventory() {
+        return inventory;
+    }
+    public int getKekenyangan() {
+        return kekenyangan;
+    }
+    public int getMood() {
+        return mood;
+    }
+    public int getKesehatan() {
+        return kesehatan;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public Ruangan getLokasi() {
+        return lokasi;
+    }
+    public boolean getIsDuduk() {
+        return isDuduk;
+    }
+    public ArrayList<Rumah> getKepemilikanRumah() {
+        return kepemilikanRumah;
+    }
+    public int getWaktuTidur() {
+        return waktuTidur;
+    }
 
     // Method : Setter
+    public void setMood(int newMood) {
+        this.mood = newMood;
+    }
+    public void setKesehatan(int newKesehatan) {
+        this.kesehatan = newKesehatan;
+    }
+    public void setKekenyangan(int newKekenyangan) {
+        this.kekenyangan = newKekenyangan;
+    }
     
-    // Method : Until Sim
+    // Method : Terkait Sim (Nonaksi)
+
+    // Method : Terkait Sim (Aksi)
+
 }
