3f94652e49c057bfa9fe75c11ca37c6535402d6f
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-04-18 20:47:59 +0700

    Update Inventory.java

--- src/entity/Inventory.java
+++ src/entity/Inventory.java
@@ -4,17 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Inventory {
+    // Deklarasi Atribut
     private Map<Objek,Integer> container;
 
+    // Konstruktor
     public Inventory() {
-        container  = new HashMap<>();
+        container = new HashMap<>();
     }
 
+    // Method
     public void addItem(Objek objek) {
-        if (container.containsKey(objek)) {
+        if (checkItem(objek)) {
             container.put(objek,container.get(objek)+1);
         } else {
             container.put(objek, 1);
+        }
+    }
+
+    public void removeItem(Objek objek) {
+        if (checkItem(objek)) {
+            if (container.get(objek) > 1) {
+                container.put(objek,container.get(objek)-1);
+            } else {
+                container.remove(objek);
+            }
+        } else {
+            System.out.println("Item tidak ada");
         }
     }
 
@@ -24,17 +39,5 @@ public class Inventory {
 
     public Map<Objek,Integer> getContainer() {
         return container;
-    }
-
-    public void removeItem(Objek objek) {
-        if (container.containsKey(objek)) {
-            if (container.get(objek) > 1) {
-                container.put(objek,container.get(objek)-1);
-            } else {
-                container.remove(objek);
-            }
-        } else {
-            System.out.println("Item tidak ada");
-        }
     }
 }
