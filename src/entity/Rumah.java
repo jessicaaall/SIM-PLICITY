7aadf4f3a9fbb8f494eb4337b59090e679853f91
Kenazizan01 <18221107@std.stei.itb.ac.id>
2023-04-21 05:39:54 +0700

    update rumah ruangan

--- src/entity/Rumah.java
+++ src/entity/Rumah.java
@@ -6,12 +6,12 @@ import java.util.*;
 
 public class Rumah  {
     private Point lokasi = new Point();
-    private ArrayList<Ruangan> daftarRuangan = new ArrayList<>();
+    private ArrayList<Ruangan> daftarRuangan = new ArrayList<Ruangan>();
     private Sim sim;
     World world;
     private Color color;
 
-    public Rumah(int x, int y, Sim sim, Color color,World world){
+    public Rumah(int x, int y, Sim sim,World world){
         this.world = world;
         if(x > world.getWidth() -1 || y > world.getHeight()-1){
             System.out.println("Titik diluar jangkauan");
@@ -20,8 +20,7 @@ public class Rumah  {
             this.sim = sim;
             lokasi.x = x;
             lokasi.y = y;
-            this.color = color;
-            daftarRuangan.add(new Ruangan(this, new Point(0,0)));
+            daftarRuangan.add(new Ruangan("Ruangan 1",this,new Point(1,1)));
         }
     }
 
@@ -41,9 +40,26 @@ public class Rumah  {
         return color;
     }
 
+    public void addRuangan(Ruangan ruangan){
+        daftarRuangan.add(ruangan);
+    }
+
+    public void removeRuangan(Ruangan ruangan){
+        Boolean sama = false;
+        for(Ruangan ruanganRumah: daftarRuangan){
+            if(ruanganRumah == ruangan){
+                sama = true;
+            }
+        }
+        if(sama){
+            daftarRuangan.remove(ruangan);
+        }
+        
+    }
+
     public void showDaftarRuangan(){
         for(Ruangan ruangan : daftarRuangan){
-            System.out.println("nama ruangan");
+            System.out.println(ruangan.getNama());
         }
     }
 }
