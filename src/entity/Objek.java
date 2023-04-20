d0ba6c828eeff9f1ddea7e8bfae6991c7d4eb9ab
Jessica <jessicalistiaraa@gmail.com>
2023-04-20 15:58:09 +0700

    hapus harga

--- src/entity/Objek.java
+++ src/entity/Objek.java
@@ -2,36 +2,29 @@ package entity;
 
 public class Objek {
     private int id;
-    private String name;
+    private String nama;
     private String jenis;
-    private int harga;
 
-    public Objek(int id, String name, String jenis, int harga) {
+    public Objek(int id, String nama, String jenis) {
         this.id = id;
-        this.name = name;
+        this.nama = nama;
         this.jenis = jenis;
-        this.harga = harga;   
     }
 
     public int getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getNama() {
+        return nama;
     }
 
     public String getJenis() {
         return jenis;
     }
-
-    public int getHarga() {
-        return harga;
-    }
-
-    @Override
+    
     public boolean equals(Object obj) {
-        Objek objek = (Objek)obj;
+        Objek objek = (Objek) obj;
         return this.id == objek.getId();
     }
 }
