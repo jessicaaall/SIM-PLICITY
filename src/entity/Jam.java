ebc16ee902b0a06b381485590babe9c933a3292c
Jessica <jessicalistiaraa@gmail.com>
2023-04-22 02:41:31 +0700

    add Jam.java

---
+++ src/entity/Jam.java
@@ -0,0 +1,18 @@
+package entity;
+
+public class Jam extends Perabotan {
+    private Waktu waktu;
+
+    public Jam(World world) {
+        super(10);
+        waktu = world.getWaktu();
+    }
+
+    public Waktu getWaktu() {
+        return waktu;
+    }
+
+    public void tampilkanJam(Sim sim) {
+
+    }
+}
