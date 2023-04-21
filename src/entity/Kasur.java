15d8fd9238537d84b60de1db9abea81b8c45c1bd
Jessica <jessicalistiaraa@gmail.com>
2023-04-22 01:52:50 +0700

    add Kasur.java

--- src/entity/Kasur.java
+++ src/entity/Kasur.java
@@ -1,8 +1,33 @@
 package entity;
 
-public class Kasur {
-    // Maap yak gw bikin filenya duluan jess, soalnya biar bisa ngerun Sim make method tidur wkwkwk
+public class Kasur extends Perabotan implements BisaDiduduki {
+    private String tipe;
+
+    public Kasur(int id) {
+        super(id);
+        if (id == 5) {
+            tipe = "single";
+        } else if (id == 6) {
+            tipe = "queen size";
+        } else if (id == 7) {
+            tipe = "king size";
+        }
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+    
     public void tidur(Sim sim, int lama) {
         
+    }
+
+    @Override
+    public void duduk(Sim sim) {
+        
+    }
+
+    public void berdiri(Sim sim) {
+
     }
 }
