fbf7fa7c69fdc53a537e44526c66bb32d421d62e
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-04-23 10:06:47 +0700

    Update Inventory.java

--- src/entity/Inventory.java
+++ src/entity/Inventory.java
@@ -9,7 +9,7 @@ public class Inventory<T> {
 
     // Konstruktor
     public Inventory() {
-        container = new HashMap<>();
+        container = new HashMap<T, Integer>();
     }
 
     // Method
@@ -18,11 +18,20 @@ public class Inventory<T> {
     }
 
     public void addItem(T item) {
+        if (checkItem(item)) {
+            container.put(item, container.get(item) + 1);
+        } else {
+            container.put(item, 1);
+        }
 
     }
     
     public void removeItem(T item) {
-
+        if (checkItem(item)) {
+            container.remove(item);
+        } else {
+            System.out.println("Tidak ada item yang dimaksud");
+        }
     }
 
     public boolean checkItem(T item) {
@@ -33,5 +42,17 @@ public class Inventory<T> {
         for (Map.Entry<T, Integer> pair : container.entrySet()) {
             System.out.println(" - " + pair.getKey() + " sejumlah " + pair.getValue() + "buah");
         }
+    }
+
+    public static void  main (String[] args) {
+        Inventory<Objek> inventory = new Inventory<Objek>();
+
+        Objek mejaKursi = new Objek("meja kursi");
+        Objek komporGas = new Objek("kompor gas");
+
+        inventory.addItem(mejaKursi);
+        inventory.addItem(komporGas);
+
+        inventory.showItem();
     }
 }
