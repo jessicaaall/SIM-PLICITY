8c2b39438df958f0ae46d46ff2c6eec8305a2953
Jessica <jessicalistiaraa@gmail.com>
2023-04-24 23:48:52 +0700

    add BakMandi.java

---
+++ src/entity/BakMandi.java
@@ -0,0 +1,42 @@
+package entity;
+
+import java.util.*;
+
+public class BakMandi extends Perabotan {
+    public BakMandi() {
+        super(35);
+    }
+
+    public void mandi(Sim sim) {
+        Scanner sc = new Scanner(System.in);
+        boolean valid = false;
+        while (!valid) {
+            System.out.print("Masukkan durasi mandi dalam satuan detik >> ");
+            try {
+                int durasi = Integer.parseInt(sc.nextLine());
+                if (durasi <= 0) {
+                    throw new IllegalArgumentException("Durasi harus lebih besar dari 0 detik.");
+                }
+                valid = true;
+                long startTime = System.currentTimeMillis();
+                long currentTime = startTime;
+                long endTime = startTime + (durasi*1000);
+                while (currentTime < endTime) {
+                    if (currentTime - startTime >= 15000) {
+                        startTime = currentTime;
+                        sim.setMood(sim.getMood() + 5);
+                        sim.setKekenyangan(sim.getKekenyangan() - 1);
+                        sim.setKesehatan(sim.getKesehatan() + 3);
+                    }
+                    currentTime = System.currentTimeMillis();
+                }
+                System.out.println("Sim " + sim.getNamaLengkap() + " selesai mandi.");
+            } catch (NumberFormatException e) {
+                System.out.println("Input tidak valid.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        sc.close();
+    }
+}
