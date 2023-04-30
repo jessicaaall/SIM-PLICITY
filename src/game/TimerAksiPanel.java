ac3577ed3e8cb3d7898ab9d90ca35b8109324707
Jessica <jessicalistiaraa@gmail.com>
2023-04-30 21:27:23 +0700

    add timer aksi panel

---
+++ src/game/TimerAksiPanel.java
@@ -0,0 +1,104 @@
+package game;
+
+import entity.*;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TimerAksiPanel extends JPanel implements MouseListener, MouseMotionListener {
+    MouseEvent pressed;
+    Point location;
+
+    HousePanel hp;
+
+    TimerAksiPanel(HousePanel hp, String aksi){
+        super(new BorderLayout());
+        this.hp = hp;
+        setLocation(hp.centerPanel.getWidth()/6-18, hp.centerPanel.getHeight()/3+30);
+        setBounds(hp.centerPanel.getWidth()/6-18, hp.centerPanel.getHeight()/3+30, 480, 150);
+        setBackground(new Color(255, 255, 255, 200));
+        setOpaque(true);
+        setBorder(new LineBorder(Color.BLACK, 3, true));
+        setFocusable(false);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(new Color(255, 255, 255, 200));
+        mainPanel.setOpaque(false);
+        mainPanel.setFocusable(false);
+
+        GridLayout layout = new GridLayout(2, 1);
+        layout.setVgap(5);
+        mainPanel.setLayout(layout);
+
+        JLabel aksiTitle = new JLabel("Sim sedang " + aksi + " ...");
+        aksiTitle.setFocusable(false);
+        aksiTitle.setVerticalAlignment(SwingConstants.CENTER);
+        aksiTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        aksiTitle.setFont(new Font("Monospaced", Font.BOLD, 20));
+      
+        JLabel timer = new JLabel(String.valueOf(100));
+        timer.setFocusable(false);
+        timer.setVerticalAlignment(SwingConstants.CENTER);
+        timer.setHorizontalAlignment(SwingConstants.CENTER);
+        timer.setFont(new Font("Monospaced", Font.BOLD, 20));
+
+        mainPanel.add(aksiTitle);
+        mainPanel.add(timer);
+
+        add(mainPanel, BorderLayout.CENTER);
+        revalidate();
+        repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        pressed = e;
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        if (pressed == null) {
+            System.out.println("null");
+            return;
+        }
+        Component component = e.getComponent();
+        location = component.getLocation();
+        int dx = (int) (location.x - pressed.getX() + e.getX());
+        int dy = (int) (location.y - pressed.getY() + e.getY());
+        component.setLocation(dx, dy);
+        component.repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+    }
+        
+}
